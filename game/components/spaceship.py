85660960effbbaa7f3f9a803a1d95846f0b42c39
Sofia Mora <Sara.Mora@fundacion-jala.org>
2023-07-13 21:38:44 -0500

    First-Class-Homework 1

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -1,5 +1,5 @@
 import pygame
-from pygame.sprite import Sprite
+from pygame.locals import *
 from game.utils.constants import SPACESHIP, SCREEN_WIDTH, SCREEN_HEIGHT
 
 class Spaceship:
@@ -8,39 +8,68 @@ class Spaceship:
         self.rect = self.image.get_rect()
         self.rect.x = 520
         self.rect.y = 500
+        self.is_w_pressed = False
+        self.is_a_pressed = False
+        self.is_d_pressed = False
         
-    def update(self, user_imput):
-        if user_imput[pygame.K_a]:
-            self.move_left()
-            print(user_imput)
-        elif user_imput[pygame.K_d]:
-            self.move_right()
-        elif user_imput[pygame.K_w]:
+    def update(self, user_input):
+        self.is_w_pressed = user_input[pygame.K_w]
+        self.is_a_pressed = user_input[pygame.K_a]
+        self.is_d_pressed = user_input[pygame.K_d]
+        
+        if self.is_a_pressed:
+            if self.is_w_pressed:
+                self.move_up_left()
+            else:
+                self.move_left()
+        elif self.is_d_pressed:
+            if self.is_w_pressed:
+                self.move_up_right()
+            else:
+                self.move_right()
+        elif self.is_w_pressed:
             self.move_up()
-        elif user_imput[pygame.K_s]:
+        elif user_input[pygame.K_s]:
             self.move_down()
             
     def move_left(self):
-        print("lef")
+        print("left")
         if self.rect.left > 50:
             self.rect.x -= 10
+        else:
+            self.rect.x = SCREEN_WIDTH - self.rect.width
     
     def move_right(self):
-        print("rig")
+        print("right")
         if self.rect.right < SCREEN_WIDTH:
             self.rect.x += 10
+        else:
+            self.rect.x = 0
             
     def move_up(self):
-        print("above")
-        if self.rect.y > SCREEN_HEIGHT // 2:
+        print("up")
+        if self.rect.top > SCREEN_HEIGHT //  2:
             self.rect.y -= 10
+            
     def move_down(self):
-        print("below")
-        if self.rect.y < SCREEN_HEIGHT - 50: 
-            self.rect.y += 10             
-        
+        print("down")
+        if self.rect.bottom < SCREEN_HEIGHT - 50:
+            self.rect.y += 10
+    
+    def move_up_left(self):
+        print("up left")
+        if self.rect.top > SCREEN_HEIGHT // 2 and self.rect.left > 50:
+            self.rect.y -= 10
+            self.rect.x -= 10
+    
+    def move_up_right(self):
+        print("up right")
+        if self.rect.top > SCREEN_HEIGHT // 2 and self.rect.right < SCREEN_WIDTH:
+            self.rect.y -= 10
+            self.rect.x += 10
+            
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
-        
+  
 
        
