b45f641ac8bc1fb61b96c0d8c24c030fba5ee365
Sofia Mora <sofiasaramoratrujillo@gamil.com>
2023-07-17 13:05:55 -0500

    Second-Class-Homework

--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,20 +1,22 @@
+import pygame
+import random
 from game.components.enemies.enemy import Enemy
+from game.components.enemies.enemy2 import Enemy2
 
 class EnemyManager:
     def __init__(self):
-        self.enemies: list[Enemy] = []
-    
+        self.enemies = []
     
     def update(self):
         if not self.enemies:
-            self.enemies.append(Enemy())
+            if random.randint(0, 1) == 0:
+                self.enemies.append(Enemy())
+            else:
+                self.enemies.append(Enemy2())
             
-        
         for enemy in self.enemies:
             enemy.update(self.enemies)
     
     def draw(self, screen):
         for enemy in self.enemies:
             enemy.draw(screen)
-      
-        
