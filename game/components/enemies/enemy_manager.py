f0c56908984905dbc7723cbb1c3a95b3f4e3a1d5
Sofia Mora <134168046+chofi-chan@users.noreply.github.com>
2023-07-17 23:18:18 -0500

    Three-Class

--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,22 +1,21 @@
-import pygame
 import random
 from game.components.enemies.enemy import Enemy
-from game.components.enemies.enemy2 import Enemy2
-
+#from game.components.enemies.enemy2 import Enemy2
 class EnemyManager:
     def __init__(self):
-        self.enemies = []
+        self.enemies: list[Enemy] = []
     
-    def update(self):
+    
+    def update(self, game):
         if not self.enemies:
-            if random.randint(0, 1) == 0:
-                self.enemies.append(Enemy())
-            else:
-                self.enemies.append(Enemy2())
+            enemy_variant = random.randint(1, 2)
+            self.enemies.append(Enemy(enemy_variant))
             
+        
         for enemy in self.enemies:
-            enemy.update(self.enemies)
+            enemy.update(self.enemies, game)
     
     def draw(self, screen):
         for enemy in self.enemies:
             enemy.draw(screen)
+        
--- game/components/game.py
+++ game/components/game.py
@@ -1,7 +1,9 @@
 import pygame
 from game.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE
+from game.components.bullets.bullet_manager import BulletManager
 from game.components.enemies.enemy_manager import EnemyManager
 from game.components.spaceship import Spaceship
+
 class Game:
     def __init__(self):
         pygame.init()
@@ -15,6 +17,7 @@ class Game:
         self.y_pos_bg = 0
         self.player = Spaceship()
         self.enemy_manager = EnemyManager()
+        self.bullet_manager = BulletManager()
     def run(self):
         # Game loop: events - update - draw
         self.playing = True
@@ -31,13 +34,15 @@ class Game:
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
-        self.enemy_manager.update()
+        self.enemy_manager.update(self)
+        self.bullet_manager.update(self)
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
         self.player.draw(self.screen)
         self.enemy_manager.draw(self.screen)
+        self.bullet_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
     def draw_background(self):
@@ -48,4 +53,4 @@ class Game:
         if self.y_pos_bg >= SCREEN_HEIGHT:
             self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg - image_height))
             self.y_pos_bg = 0
-
+        self.y_pos_bg += self.game_speed
